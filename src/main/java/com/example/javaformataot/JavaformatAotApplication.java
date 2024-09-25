d80c162bc1f348e931c5deb44dc9ac1743ebca63
Josh Cummings <josh.cummings@gmail.com>
2024-09-25 13:45:12 -0600

    Sample

---
+++ src/main/java/com/example/javaformataot/JavaformatAotApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaformataot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaformatAotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaformatAotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaformataot/JavaformatAotApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.javaformataot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaformatAotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
